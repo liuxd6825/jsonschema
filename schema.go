7dcfd7d82c72eba77e801aac8669944a889fe914
duxm0114 <duxm0114@163.com>
2024-12-01 16:31:14 +0800

    增加json标签

--- schema.go
+++ schema.go
@@ -10,89 +10,89 @@ import (
 // Schema is the regpresentation of a compiled
 // jsonschema.
 type Schema struct {
-	up                urlPtr
-	resource          *Schema
-	dynamicAnchors    map[string]*Schema
-	allPropsEvaluated bool
-	allItemsEvaluated bool
-	numItemsEvaluated int
+	up                urlPtr             `json:"up"`
+	resource          *Schema            `json:"resource,omitempty"`
+	dynamicAnchors    map[string]*Schema `json:"dynamicAnchors,omitempty"`
+	allPropsEvaluated bool               `json:"allPropsEvaluated,omitempty"`
+	allItemsEvaluated bool               `json:"allItemsEvaluated,omitempty"`
+	numItemsEvaluated int                `json:"numItemsEvaluated,omitempty"`
 
-	DraftVersion int
-	Location     string
+	DraftVersion int    `json:"draftVersion" json:"draftVersion,omitempty"`
+	Location     string `json:"location" json:"location,omitempty"`
 
 	// type agnostic --
-	Bool            *bool // boolean schema
-	ID              string
-	Ref             *Schema
-	Anchor          string
-	RecursiveRef    *Schema
-	RecursiveAnchor bool
-	DynamicRef      *DynamicRef
-	DynamicAnchor   string // "" if not specified
-	Types           *Types
-	Enum            *Enum
-	Const           *any
-	Not             *Schema
-	AllOf           []*Schema
-	AnyOf           []*Schema
-	OneOf           []*Schema
-	If              *Schema
-	Then            *Schema
-	Else            *Schema
-	Format          *Format
+	Bool            *bool       `json:"bool,omitempty"` // boolean schema
+	ID              string      `json:"ID,omitempty"`
+	Ref             *Schema     `json:"ref,omitempty"`
+	Anchor          string      `json:"anchor,omitempty"`
+	RecursiveRef    *Schema     `json:"recursiveRef,omitempty"`
+	RecursiveAnchor bool        `json:"recursiveAnchor,omitempty"`
+	DynamicRef      *DynamicRef `json:"dynamicRef,omitempty"`
+	DynamicAnchor   string      `json:"dynamicAnchor,omitempty"` // "" if not specified
+	Types           *Types      `json:"types,omitempty"`
+	Enum            *Enum       `json:"enum,omitempty"`
+	Const           *any        `json:"const,omitempty"`
+	Not             *Schema     `json:"not,omitempty"`
+	AllOf           []*Schema   `json:"allOf,omitempty"`
+	AnyOf           []*Schema   `json:"anyOf,omitempty"`
+	OneOf           []*Schema   `json:"oneOf,omitempty"`
+	If              *Schema     `json:"if,omitempty"`
+	Then            *Schema     `json:"then,omitempty"`
+	Else            *Schema     `json:"else,omitempty"`
+	Format          *Format     `json:"format,omitempty"`
 
 	// object --
-	MaxProperties         *int
-	MinProperties         *int
-	Required              []string
-	PropertyNames         *Schema
-	Properties            map[string]*Schema
-	PatternProperties     map[Regexp]*Schema
-	AdditionalProperties  any            // nil or bool or *Schema
-	Dependencies          map[string]any // value is []string or *Schema
-	DependentRequired     map[string][]string
-	DependentSchemas      map[string]*Schema
-	UnevaluatedProperties *Schema
+	MaxProperties         *int                `json:"maxProperties,omitempty"`
+	MinProperties         *int                `json:"minProperties,omitempty"`
+	Required              []string            `json:"required,omitempty"`
+	PropertyNames         *Schema             `json:"propertyNames,omitempty"`
+	Properties            map[string]*Schema  `json:"properties,omitempty"`
+	PatternProperties     map[Regexp]*Schema  `json:"patternProperties,omitempty"`
+	AdditionalProperties  any                 `json:"additionalProperties,omitempty"` // nil or bool or *Schema
+	Dependencies          map[string]any      `json:"dependencies,omitempty"`         // value is []string or *Schema
+	DependentRequired     map[string][]string `json:"dependentRequired,omitempty"`
+	DependentSchemas      map[string]*Schema  `json:"dependentSchemas,omitempty"`
+	UnevaluatedProperties *Schema             `json:"unevaluatedProperties,omitempty"`
 
 	// array --
-	MinItems         *int
-	MaxItems         *int
-	UniqueItems      bool
-	Contains         *Schema
-	MinContains      *int
-	MaxContains      *int
-	Items            any // nil or []*Schema or *Schema
-	AdditionalItems  any // nil or bool or *Schema
-	PrefixItems      []*Schema
-	Items2020        *Schema
-	UnevaluatedItems *Schema
+	MinItems         *int      `json:"minItems,omitempty"`
+	MaxItems         *int      `json:"maxItems,omitempty"`
+	UniqueItems      bool      `json:"uniqueItems,omitempty"`
+	Contains         *Schema   `json:"contains,omitempty"`
+	MinContains      *int      `json:"minContains,omitempty"`
+	MaxContains      *int      `json:"maxContains,omitempty"`
+	Items            any       `json:"items,omitempty"`           // nil or []*Schema or *Schema
+	AdditionalItems  any       `json:"additionalItems,omitempty"` // nil or bool or *Schema
+	PrefixItems      []*Schema `json:"prefixItems,omitempty"`
+	Items2020        *Schema   `json:"items2020,omitempty"`
+	UnevaluatedItems *Schema   `json:"unevaluatedItems,omitempty"`
 
 	// string --
-	MinLength        *int
-	MaxLength        *int
-	Pattern          Regexp
-	ContentEncoding  *Decoder
-	ContentMediaType *MediaType
-	ContentSchema    *Schema
+	MinLength        *int       `json:"minLength,omitempty"`
+	MaxLength        *int       `json:"maxLength,omitempty"`
+	Pattern          Regexp     `json:"pattern,omitempty"`
+	ContentEncoding  *Decoder   `json:"contentEncoding,omitempty"`
+	ContentMediaType *MediaType `json:"contentMediaType,omitempty"`
+	ContentSchema    *Schema    `json:"contentSchema,omitempty"`
 
 	// number --
-	Maximum          *big.Rat
-	Minimum          *big.Rat
-	ExclusiveMaximum *big.Rat
-	ExclusiveMinimum *big.Rat
-	MultipleOf       *big.Rat
+	Maximum          *big.Rat `json:"maximum,omitempty"`
+	Minimum          *big.Rat `json:"minimum,omitempty"`
+	ExclusiveMaximum *big.Rat `json:"exclusiveMaximum,omitempty"`
+	ExclusiveMinimum *big.Rat `json:"exclusiveMinimum,omitempty"`
+	MultipleOf       *big.Rat `json:"multipleOf,omitempty"`
 
-	Extensions []SchemaExt
+	Extensions []SchemaExt `json:"extensions,omitempty"`
 
 	// annotations --
-	Title       string
-	Description string
-	Default     *any
-	Comment     string
-	ReadOnly    bool
-	WriteOnly   bool
-	Examples    []any
-	Deprecated  bool
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	Default     *any   `json:"default,omitempty"`
+	Comment     string `json:"comment,omitempty"`
+	ReadOnly    bool   `json:"readOnly,omitempty"`
+	WriteOnly   bool   `json:"writeOnly,omitempty"`
+	Examples    []any  `json:"examples,omitempty"`
+	Deprecated  bool   `json:"deprecated,omitempty"`
 }
 
 // --
@@ -110,6 +110,21 @@ const (
 	objectType
 	dateType
 	dateTimeType
+)
+
+type JsonType = jsonType
+
+const (
+	JsonType_InvalidType  = invalidType
+	JsonType_NullType     = nullType
+	JsonType_BooleanType  = booleanType
+	JsonType_NumberType   = numberType
+	JsonType_IntegerType  = integerType
+	JsonType_StringType   = stringType
+	JsonType_ArrayType    = arrayType
+	JsonType_ObjectType   = objectType
+	JsonType_DateType     = dateType
+	JsonType_DateTimeType = dateTimeType
 )
 
 type ISchemaType interface {
@@ -228,6 +243,10 @@ func (tt *Types) add(t jsonType) {
 func (tt Types) contains(t jsonType) bool {
 	val := int(tt)&int(t) != 0
 	return val
+}
+
+func (tt Types) Contains(t jsonType) bool {
+	return tt.contains(t)
 }
 
 func (tt Types) ToStrings() []string {
